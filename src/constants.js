38ddd967606d3b6b55f979009dee1df492122a30
Kristina Tripak <tinatripak2002@gmail.com>
2023-11-23 22:12:57 +0200

    changed baseUrl

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const sizeWidth = 880;
 
-export const baseURL = "http://localhost:8000/";
+export const baseURL = "https://photograph-portfolio-service.onrender.com/";
 
 export const emptyLink = "/";
 export const loginLink = "/login";
